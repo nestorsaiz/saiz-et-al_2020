2f3b29ed1e1caf3e1d9734f72e714bc216a02a87
nestorsaiz <nestorsaiz@icloud.com>
2019-11-15 10:41:23 -0500

    annotated and tested

--- src/checkols.R
+++ src/checkols.R
@@ -1,6 +1,8 @@
 ## This little function compares column names between two given datasets
 ## (data1 and data2), checking which variables of data1 are NOT in data2
 
+## Just a very lazy function to avoid typing multiple parenthesis, brackets etc
+
 checkols <- function(data1, data2) {
         diffs <- colnames(data1)[which(!colnames(data1) %in% colnames(data2))]
         print(diffs)
