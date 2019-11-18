6b03b925aa10d4a635550f544ac525d8f07be26a
nestorsaiz <nestorsaiz@icloud.com>
2019-11-18 10:34:12 -0500

    first commit

---
+++ src/setup.R
@@ -0,0 +1,11 @@
+# This script loads a handful of required packages for all scripts
+# as well as the 'plotting-aes.R' file with some objects I use for plotting
+
+library('packrat')
+library('reshape2')
+library("plyr")
+library("dplyr")
+library("ggplot2")
+library('RColorBrewer')
+
+source('./src/plotting-aes.R')
