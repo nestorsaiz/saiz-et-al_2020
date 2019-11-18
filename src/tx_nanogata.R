f48146887dd98733dfa46c6e526b06aae2250c9b
nestorsaiz <nestorsaiz@icloud.com>
2019-11-18 14:35:54 -0500

    made into more flexible function. Not tested

--- src/tx_nanogata.R
+++ src/tx_nanogata.R
@@ -1,38 +1,61 @@
-tx.nanogata <- function(dataset) { 
-        ## Source script modelling the relationship betweeen 
-        ## NANOG.rat vs NANOG.rb and GATA6.rb vs GATA6.gt 
-        source('abtest.R')
-        
-        ## Split dataset in embryos stained for NANOG.rat and embryos which aren't
-        rat <- subset(dataset, Experiment %in% ng.rat)
-        norat <- subset(dataset, !Experiment %in% ng.rat)
-        
-        ## Transform CH2.ebLogCor in rat subset using intercept and slope from ng.model
-        ## obtained in abtest.R and make equivalent column in norat subset
-        ## where the values are the same as CH3.ebLogCor (NANOG.rb).
-        ## Call the transformed value CH3.ebLogCor.x, as it will be equivalent to 
-        ## CH3.ebLogCor, where NANOG.rb values are found.
-        rat$CH3.ebLogCor.x <- (rat$CH2.ebLogCor - ng.model$coefficients[1])/ 
-                ng.model$coefficients[2]
-        norat$CH3.ebLogCor.x <- norat$CH3.ebLogCor
-        ## Combine both subsets into new.lms
-        dataset <- rbind(rat, norat)
-        rm(rat, norat)
-        
-        ## Split dataset in embryos stained for GATA6.rb and embryos which aren't
-        rb <- subset(dataset, Experiment %in% g6.rb)
-        norb <- subset(dataset, !Experiment %in% g6.rb)
-        
-        ## Transform CH3.ebLogCor in rb subset using intercept and slope from gata.model
-        ## obtained in abtest.R and make equivalent column in norb subset
-        ## where the values are the same as CH5.ebLogCor
-        rb$CH5.ebLogCor.x <- (rb$CH3.ebLogCor - gata.model$coefficients[1])/ 
-                gata.model$coefficients[2]
-        norb$CH5.ebLogCor.x <- norb$CH5.ebLogCor
-        ## Combine both subsets into new.lms
-        dataset <- rbind.fill(rb, norb)
-        rm(rb, norb)
-        
-        ## Return the transformed dataset
-        return(dataset)
+# This function transforms fluorescence values in a given dataset
+# using a regression model previously calculated.
+# The function takes a number of arguments (whith no defaults):
+# * dataset: data with the channel to be transformed
+# * what.subset: a vector of experiments which defines 
+#   the subset of data to be transformed
+# * what.model: the regression model to be used for the linear transformation
+# * input.ch: the channel to be transformed, either as a number (1-5) or
+#   in the form of a string "CHx", with x being the number of the channel (1-5)
+# * end.ch: the channel to which we want to make input.ch equivalent,
+#   in the same format as input.ch
+#   (for instance, if we want to transform CH2 into CH3-equivalent values, 
+#   input.ch is CH2, and end.ch is CH3)
+
+# Check if model has already been loaded and if not, 
+# run ab-test.R, which generates the models
+model.exsts <- exists('ng.model')
+if(model.exsts == F) { 
+        source('./src/ab-test.R')
+}
+rm(model.exsts)
+
+
+tx.nanogata <- function(dataset, what.subset, what.model, 
+                        input.ch, end.ch) { 
+        # Format input and end channel as "CHx"
+        if (is.numeric(input.ch) == TRUE) {
+                input.ch <- paste('CH', input.ch, sep = '')
         }
+        if (is.numeric(end.ch) == TRUE) {
+                end.ch <- paste('CH', end.ch, sep = '')
+        }
+        # Append the desired termination to the channels to match data tables
+        input.ch <- paste(input.ch, '.ebLogCor', sep = '')
+        intact.ch <- paste(end.ch, '.ebLogCor', sep = '')
+        output.ch <- paste(end.ch, '.ebLogCor.x', sep = '')
+        
+        # Split dataset in data to be transformed and data not to be trasnformed
+        tx <- subset(dataset, Experiment %in% what.subset)
+        notx <- subset(dataset, !Experiment %in% what.subset)
+        
+        # Transform channel in tx using intercept and slope from given model
+        # (what.model)
+        tx.col <- (tx[input.ch] - what.model$coefficients[1]) / 
+                what.model$coefficients[2]
+        tx.col <- data.frame(tx.col)
+        colnames(tx.col) <- output.ch
+        
+        # For values in notx, duplicate channel appending .x
+        notx.col <- notx[intact.ch]
+        notx.col <- data.frame(notx.col)
+        colnames(notx.col) <- output.ch
+        
+        # Combine each transformed variable with corresponding data subset
+        tx <- cbind(tx, tx.col)
+        notx <- cbind(no.tx, notx.col)
+        
+        # Combine both subsets and return the transformed dataset
+        dataset <- rbind(tx, notx)
+        return(dataset)
+}
