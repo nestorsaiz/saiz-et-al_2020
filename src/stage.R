67fcf13f81051372766003bb7984b93ec0ba31cf
nestorsaiz <nestorsaiz@icloud.com>
2019-11-18 11:46:48 -0500

    made universal

--- src/stage.R
+++ src/stage.R
@@ -1,6 +1,16 @@
-stage <- function(dataset, bin = NULL){
-        ## Given a certain dataset, stage embryos according to their cell count
-        ## 1. If no intervals are given, default to our standard
+# This function takes in a dataset and stages categorizes entries 
+# (cells and embryos) according to their total cell count. 
+# It defaults to the bins we typically use, namely 8-16, 16-32, 32-64, 64-90, 
+# 90-120, 120-150, >150 cells, as in our previous papers
+# but it can take in any integer indicating bin size, in number of cells.
+
+# Optionally, it can also take in the name of the variable containing the
+# total number of cells - defaults to 'Cellcount'.
+
+stage <- function(dataset, count.var = 'Cellcount',
+                  bin = NULL){
+        dataset$Cellcount <- get(count.var, dataset)
+        # 1. If no intervals are given, default to our standard
         if(missing(bin)) {
                 cuts <- c(1, 8, 16, 32, 64, 90, 120, 150, Inf)
                 ## 2. Label the stages based on default intervals
