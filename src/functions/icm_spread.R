a5d1a3ff480b952423ee3945f7c07cae5bdacf44
nestorsaiz <nestorsaiz@icloud.com>
2019-11-19 15:21:44 -0500

    will not take channels explicitly

--- src/functions/icm_spread.R
+++ src/functions/icm_spread.R
@@ -7,16 +7,16 @@
 # It requires ggplot2
 library('ggplot2')
 
-icm.spread <- function(dataset, x, y) {
-  ol <- ggplot(dataset, aes(x = dataset[x], y = dataset[y]))
-  ol <- geom_jitter(aes(color = Cellcount), size = 2, alpha = 0.6) + 
+icm.spread <- function(dataset) {
+  ol <- ggplot(dataset, aes(x = dataset[, 2], y = dataset[, 3]))
+  ol <- ol + geom_jitter(aes(color = Cellcount), size = 2, alpha = 0.6) + 
     geom_density_2d()
   ol <- ol + theme_bw() + 
+    scale_color_distiller(direction = 1, palette = 'Blues') + 
     theme(aspect.ratio = 1, 
           axis.text = element_text(size = 15), 
           axis.title = element_text(size = 20), 
           legend.text = element_text(size = 15), 
-          legend.title = element_text(size = 20)) + 
-    scale_color_distiller(direction = 1, palette = 'Blues')
+          legend.title = element_text(size = 20))
   print(ol)
 }
