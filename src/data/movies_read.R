2e65031f3bcf717090cac5fb27a702869cddde20
nestorsaiz <nestorsaiz@icloud.com>
2019-11-22 10:47:38 -0500

    reads in files, cleans data and writes out table

--- src/data/movies_read.R
+++ src/data/movies_read.R
@@ -1,5 +1,7 @@
-# This script reads in the files from time lapse movies
-
+# This script reads in the files for time lapse movies 
+# from a collection of folders, binds them together, 
+# cleans the data, extracts information for each cell from a 'labels' string
+# and writes out a tidy .csv with all movie data
 
 # Check that setup.R has been ran
 setup.ran <- exists('looks')
@@ -92,4 +94,112 @@ for(m in 1:length(folders)) {
 
 # Print out the cycle counter
 print(paste('Number of files loaded:', cycles, sep = ' '))
+
+# Cells which only have one timeframe because they die right away
+# have no TrackID assigned. This can cause problems later when counting cells
+# Correct this by giving them a TrackID that consists of the number
+# 100000 + the corresponding spot ID number
+for(m in 1:length(movies)) {
+  bien <- subset(movies[[m]], is.na(TrackID) == F)
+  mal <- subset(movies[[m]], is.na(TrackID) == T)
+  mal$TrackID <- mal$ID + 100000
+  movies[[m]] <- rbind(bien, mal)
+}
+
+# Extract information stored in the labels variable (NS.labs), such as cell
+# type, generation, division, etc., and make new variables for each label
+for(m in 1:length(movies)) { 
+  movies[[m]]$identity_t0 <- ifelse(grepl('PRE', movies[[m]]$NS.labs), 'PRE', 
+                                    ifelse(grepl('EPI', movies[[m]]$NS.labs), 'EPI', 
+                                           ifelse(grepl('DP', movies[[m]]$NS.labs), 'DP', 
+                                                  'unknown')))
+  movies[[m]]$cell_treatment <- ifelse(grepl('targeted', movies[[m]]$NS.labs), 
+                                       'targeted', 'intact')
+  movies[[m]]$TrackID <- paste(movies[[m]]$TrackID, 
+                               movies[[m]]$identity_t0,
+                               movies[[m]]$cell_treatment, sep = '.')
+  movies[[m]]$genx <- ifelse(grepl('Granddaughter', movies[[m]]$NS.labs), 'gd', 
+                             ifelse(grepl('d1', movies[[m]]$NS.labs) | 
+                                      grepl('d2', movies[[m]]$NS.labs), 'd', 'm'))
+  movies[[m]]$gd.branch <- ifelse(grepl('Granddaughter1', movies[[m]]$NS.labs), 0.1, 
+                                  ifelse(grepl('Granddaughter2', movies[[m]]$NS.labs), 0.2, 0))
+  movies[[m]]$d.branch <- ifelse(grepl('d1', movies[[m]]$NS.labs), 1, 
+                                 ifelse(grepl('d2', movies[[m]]$NS.labs), 2, 0))
+  movies[[m]]$branch <- movies[[m]]$d.branch + movies[[m]]$gd.branch
+  movies[[m]]$division <- ifelse(grepl('Mito', movies[[m]]$NS.labs), TRUE, FALSE)
+  movies[[m]]$death <- ifelse(grepl('Apoptosis', movies[[m]]$NS.labs), TRUE, FALSE)
+  movies[[m]]$in.frame <- ifelse(grepl('off', movies[[m]]$NS.labs), FALSE, TRUE)
+  movies[[m]]$Cell_ID <- paste(movies[[m]]$TrackID, movies[[m]]$genx, 
+                               movies[[m]]$branch, sep = '.')
+  movies[[m]]$d.branch <- NULL
+  movies[[m]]$gd.branch <- NULL
+}
+
+# Rename embryos, which currently have the file name, 
+# to match the format in the main ablation experimental reference file
+for(m in 1:length(movies)) { 
+  # Break Embryo_ID content into components
+  parts <- strsplit(movies[[m]]$Embryo_ID, split = '_')
+  # Reformat date from the existing DDMonYY to MMDDYY
+  my.date <- as.Date(parts[[1]][1], '%d%b%y')
+  my.date <- format(my.date, '%m%d%y')
+  # Reformat as MMDDYY + Abl + _ + Litter + Embryo number
+  ee <- paste(paste(my.date, 'Abl', sep = ''), parts[[1]][3], sep = '_')
+  movies[[m]]$Embryo_ID <- ee
+}
+
+## Rbind all elements of movies into a data frame
+movies <- do.call(rbind, movies)
+
+# Make TrackID and Cell_ID unique by incorporating the corresponding embryo name
+movies <- rename(movies, spot_ID = ID)
+movies$TrackID <- paste(movies$Embryo_ID, movies$TrackID, sep = '.')
+movies$Cell_ID <- paste(movies$Embryo_ID, movies$Cell_ID, sep = '.')
+
+# Add TE vs ICM variable
+movies$TE_ICM <- 'ICM'
+
+# Log and print out the actual number of movies loaded
+# (there are more than one file per movie/embryo)
+n.movies <- unique(movies$Embryo_ID)
+print(paste('Number of movies loaded:', length(n.movies), sep = ' '))
+
+################################################################################
+# Add metadata
+################################################################################
+
+# Read in experimental reference
+ablat.ref <- read.csv('./references/ablat_exp_ref.csv')
+ablat.t0.stage <- read.csv('./references//ablat_t0-stage.csv')
+
+# Extract data from ablat.ref only for embryos contained in ablat.movs dataset
+# and extract just some of the variables in it
+ablat.ref <- subset(ablat.ref, 
+                        Embryo_ID %in% unique(movies$Embryo_ID))
+ablat.ref <- ablat.ref %>% 
+  select(Experiment, Litter, Embryo_ID, 
+         Treatment, Cellcount_t0, 
+         Gene1, Genotype1, Gene2, Genotype2, 
+         target, Cell_diff, Recovery)
+ablat.ref <- merge(ablat.ref, ablat.t0.stage)
+rm(ablat.t0.stage)
+
+# Combine into one set and remove exp.ref
+movies <- merge(movies, ablat.ref)
+rm(ablat.ref)
+
+# Rename targets so controls are assigned 'none' as target cell
+movies$target[which(movies$Treatment == 'Control')] <- 'none'
+
+# Calculate number of embryos for each category and write to file
+n.embryos <- movies %>% 
+  group_by(Embryo_ID, target, Stage.t0, Treatment, Cell_diff) %>% 
+  summarize() %>% 
+  group_by(target, Stage.t0, Treatment, Cell_diff) %>% 
+  summarize(N = n())
+write.csv(n.embryos, file = './results/movies_N-embryos.csv', row.names = F)
+
+################################################################################
+# Write out tidy table to use in other applications
+write.csv(movies, file = './data/raw/movies-all.csv', row.names = F)
 
