c5b83ee90ab5241e45b315014504a8da887e09b0
nestorsaiz <nestorsaiz@icloud.com>
2019-11-23 17:29:08 -0500

    functional and annotated

--- src/data/emb-xim_classify.R
+++ src/data/emb-xim_classify.R
@@ -140,7 +140,132 @@ g6.chimeras$cell_type <- factor(g6.chimeras$cell_type,
                                 levels = c('host', 'donor'))
 
 ################################################################################
-## Assign lineage identity #####################################################
+# Assign lineage identity using K-means clustering
 ################################################################################
 
+# Define the miniset of the data that will be used to perform K-means
+# (cell counts, NANOG and GATA6 intensity values).
+# I exclude values from Gata6-/- cells, as they have a lot of NANOG-low
+# cells and pull the EPI cluster away from its center in wild types
+gg <- g6.chimeras %>% filter(TE_ICM == 'ICM', 
+                             !H_genotype %in% c('ko', 'mosaic')) %>% 
+  select(Cellcount, CH5.ebLogCor, CH3.ebLogCor)
+
+# Visualize distribution of NANOG and GATA6 values in ICM cells
+icm.spread(gg)
+
+# Define a vector of identities - this should run on the provided dataset 
+# but may need to be changed, if new data is added. 
+id.vector <- c('EPI', 'PRE', 'DP', 'DN')
+
+# Run mkay() to perform k-means and classify cells according to id.vector
+g6.chimeras <- mkay(dataset = g6.chimeras, miniset = gg, 
+                    k = 2, ids = id.vector,
+                    DP = T, x.var = 'CH5.ebLogCor', y.var = 'CH3.ebLogCor')
+
+# Uncomment below to visualize the result
+# qplot(CH5.ebLogCor,  CH3.ebLogCor,
+#       data = subset(g6.chimeras, TE_ICM == 'ICM'),
+#       color = Identity.km) + scale_color_manual(values = idcols) +
+#   facet_grid(Treatment ~ H_genotype) + looks + theme(aspect.ratio = 1)
+
+################################################################################
+# Assign lineage identity using Hierarchical clustering
+################################################################################
+
+# Separate TE and ICM cells. TE cells are manually assigned to cluster 0,
+# with Identity.hc == 'TE'
+te <- subset(g6.chimeras, TE_ICM == 'TE')
+icm <- subset(g6.chimeras, TE_ICM == 'ICM')
+
+te$id.cluster <- 0
+te$Identity.hc <- 'TE'
+
+# Perform hierarchical clustering with ICM cells using average linkage (UPGMA)
+# Classify separately KO cells and all other genotypes, 
+# as doing all together results in the non-ko genotypes pulling too many
+# boundary ko cells towards the PrE cluster (Gata6-/- don't have PrE)
+icm$grouping <- ifelse(icm$H_genotype != 'ko', 'NK', 'K')
+icm <- split(icm, as.factor(icm$grouping))
+
+# Loop through the list of two elements doing the same operation on each.
+# Break into 4 and 5 clusters. No DP appear in KO cells though
+ks <- c(4, 5)
+for(i in 1:2) { 
+  my.clusters <- hclust(dist(data.frame(icm[[i]]$CH5.ebLogCor, 
+                                        icm[[i]]$CH3.ebLogCor)), 
+                        method = 'average')
+  # Uncomment below to plot dendrogram
+  # plot(my.clusters)
+  icm[[i]]$id.cluster <- cutree(my.clusters, ks[i])
+  my.plot <- qplot(CH5.ebLogCor,  CH3.ebLogCor,
+                   data = icm[[i]], color = id.cluster) +
+    looks + scale_color_gradient2(low = 'black', mid = 'green',
+                                  high = 'yellow',
+                                  midpoint = (ks[i]+1)/2) +
+    facet_wrap(H_genotype ~ cell_type) + theme(aspect.ratio = 1)
+  # Uncomment below to print the outcome as scatter plots
+  # print(my.plot)
+}
+
+# Assign identities to each cluster
+idxclust <- list(data.frame(id.cluster = 1:ks[1], 
+                            Identity.hc = c('EPI', 'EPI.lo', 'PRE', 'EPI')), 
+                 data.frame(id.cluster = 1:ks[2], 
+                            Identity.hc = c('DP', 'EPI', 'PRE', 'PRE', 
+                                            'EPI.lo')))
+
+# Merge identities with icm dataset
+for(e in 1:length(icm)) { 
+  icm[[e]] <- merge(icm[[e]], idxclust[[e]])
+}
+icm <- do.call(rbind, icm)
+icm$grouping <- NULL
+
+# Combine TE and ICM cells again
+g6.chimeras <- rbind(te, icm)
+rm(te, icm)
+
+# Uncomment below to visualize the result
+# qplot(CH5.ebLogCor,  CH3.ebLogCor,
+#       data = subset(g6.chimeras, TE_ICM == 'ICM'), color = Identity.hc) +
+#   looks + scale_color_manual(values = idcols) +
+#   facet_wrap(H_genotype ~ cell_type) + theme(aspect.ratio = 1)
+
+# A few Gata6-/- cells (18) are classified as PrE cells. 
+# Some of these cells were checked visually and they're actually wild type
+# GFP+ cells with very faint GFP.
+# As can be seen on the plot below, these cells show higher GATA6 than the rest
+# of GFP- mutant EPI cells, but their GFP levels are comparable
+# to the GFP- mutant cells and were mistakenly classified as GFP-.
+
+# Uncomment below to generate plot
+# qplot(CH5.ebLogCor, CH2.logCor, 
+#       data = subset(g6.chimeras, H_genotype == 'ko' & 
+#                       cell_type == 'host' & TE_ICM == 'ICM'), 
+#       geom = 'jitter', color = Identity.hc) + looks + 
+#   scale_color_manual(values = idcols) + theme(aspect.ratio = 2)
+
+# Therefore, Gata6 ko (GFP-, host) cells classified as PrE, with GFP (CH2.logCor)
+# values > 5.25 will be re-classified as GFP+ (donor), those with values <5.25
+# will be left as GFP- but assigned to EPI - those cells are also on the edge
+# of the EPI cluster when looking at log[GATA6] vs log[NANOG]
+aa <- subset(g6.chimeras, H_genotype == 'ko' & 
+               cell_type == 'host' & Identity.hc == 'PRE')
+bb <- subset(g6.chimeras, H_genotype != 'ko' | 
+               cell_type != 'host' | Identity.hc != 'PRE')
+aa$cell_type <- ifelse(aa$CH2.logCor > 5.25, 'donor', 'host')
+aa$Identity.hc <- ifelse(aa$CH2.logCor < 5.25, 'EPI', 'PRE')
+g6.chimeras <- rbind(aa, bb)
+rm(aa, bb)
+
+# Make Identity.hc a factor and order it
+g6.chimeras$Identity.hc <- factor(g6.chimeras$Identity.hc, 
+                                  levels = c('TE', 'PRE', 'DP', 
+                                             'EPI', 'EPI.lo', 'DN'))
+
+################################################################################
+# Write out data to disk
+write.csv(g6.chimeras, file = './data/processed/emb-xim-processed.csv', 
+          row.names = F)
 
