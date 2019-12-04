795f7f672fc1a81a1fdc44b633a6a4951a9ada1b
nestorsaiz <nestorsaiz@icloud.com>
2019-12-04 11:54:18 -0500

    first commit. Functional

---
+++ src/data/gatherer.R
@@ -0,0 +1,56 @@
+# This script reads in Littermates data from the studies by
+# Saiz *et al* (2016) in *Nature Communications* and by
+# Morgani *et al* (2018) in *Developmental Biology*
+
+# Check if setup.R has been ran
+setup.ran <- exists('looks')
+if (setup.ran == F) { 
+  source('./src/setup.R')
+}
+rm(setup.ran)
+
+# Load extra packages
+library('RCurl')
+
+# Read .csv of transformed data for Saiz et al. (2016) from its GitHub repo
+my.path <- getURL('https://raw.githubusercontent.com/nestorsaiz/saiz-et-al_2016/master/FGF_all_pooled_trans.csv')
+ncoms.all <- read.csv(text = my.path)
+
+# Incorporate experiment and imaging date, from experimental reference file, 
+# which are missing for some reason.
+
+# Read file from GitHub
+my.path <- getURL('https://raw.githubusercontent.com/nestorsaiz/saiz-et-al_2016/master/FGFonCD1_exp_ref.csv')
+ncoms.ref <- read.csv(text = my.path)
+# Extract Exp_date and Img_date for each experiment present in main table
+ncoms.ref <- ncoms.ref %>% 
+  filter(Experiment %in% unique(ncoms.all$Experiment)) %>% 
+  group_by(Experiment, Exp_date, Img_date) %>% 
+  summarize()
+
+# Merge into main data frame
+ncoms.all <- merge(ncoms.all, ncoms.ref)
+rm(ncoms.ref)
+
+# Extract Littermates only from the dataset and write them out to disk
+ncoms.lms <- subset(ncoms.all, Treatment == 'Littermate')
+write.csv(ncoms.lms, file = './data/raw/ncoms-lms-raw.csv', row.names = F)
+
+
+# Read .csv of transformed data for Morgani et al. (2018) from is GitHub repo
+my.path <- getURL('https://raw.githubusercontent.com/nestorsaiz/morgani-et-al_2018/master/spry-all-processed.csv')
+spry.all <- read.csv(text = my.path)
+
+# Extract Littermates only stained for NANOG and GATA6 and drop
+# embryos which are not wild type for other alleles than Spry4
+spry.lms <- subset(spry.all, Treatment == 'Littermate' & 
+                     red.marker == 'GATA6.gt' & 
+                     farred.marker == 'NANOG.rb' & 
+                     Genotype1 != 'unknown' & 
+                     Genotype2 == 'wt')
+
+# Write Spry4 littermates out to disk
+write.csv(spry.lms, file = './data/raw/spry4-lms-raw.csv', row.names = F)
+
+
+##
